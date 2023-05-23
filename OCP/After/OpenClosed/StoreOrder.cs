e16560388740be7d0447fc9af8e35613c9f3f391
Nikitas Meggos <meggosni@live.com>
2023-05-23 18:43:46 -0500

    initial OCP scaffold

---
+++ OCP/After/OpenClosed/StoreOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenClosed;
+
+public class StoreOrder : Order
+{
+    public override void Checkout()
+    {
+        TotalAmount = SubTotalAmount;
+        Console.WriteLine($"Order {Id} for {CustomerName} has been checked out.");
+        Console.WriteLine($"Subtotal: {SubTotalAmount:C}");
+        Console.WriteLine($"Total: {TotalAmount:C}{Environment.NewLine}");
+    }
+}
