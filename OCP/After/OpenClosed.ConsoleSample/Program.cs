bcda1e7e2a5d11bb041e39bb1d7677def06d5f0e
Nikitas Meggos <meggosni@live.com>
2023-05-23 19:34:37 -0500

    refactoring OCP to use interface instead of abstract class

--- OCP/After/OpenClosed.ConsoleSample/Program.cs
+++ OCP/After/OpenClosed.ConsoleSample/Program.cs
@@ -1,6 +1,6 @@
 ﻿using OpenClosed;
 
-Order storeOrder = new StoreOrder()
+IOrder storeOrder = new StoreOrder()
 {
     Id = 1, 
     CustomerName = "John Doe", 
@@ -9,11 +9,12 @@ Order storeOrder = new StoreOrder()
 
 storeOrder.Checkout();
 
-Order onlineOrder = new OnlineOrder()
+IOrder onlineOrder = new OnlineOrder()
 {
     Id = 2,
     CustomerName = "Jane Doe",
-    SubTotalAmount = 100
+    SubTotalAmount = 100,
+    Discount = 0.20m
 };
 
 onlineOrder.Checkout();
